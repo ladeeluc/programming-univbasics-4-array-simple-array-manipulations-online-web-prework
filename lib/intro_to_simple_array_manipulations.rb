b99abcfd15c1fa2ac6644eacce0333f178c13774
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-19 18:18:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,18 +3,22 @@ def using_push (array,string)
   array.push(string)
 end
 
-def using_unshift (array,string)
+def using_unshift (Array,String)
   array.unshift(string)
 end
 
-def using_pop(array) # defined method
+def using_pop(Array) # defined method
    array.pop # took element out of array
 end
 
-def pop_with_args(array) #defined methods
+def pop_with_args(Array) #defined methods
   array.pop(2) # removes last element of  array
 end
 
-def using_unshift(array)
-  array.unshift(array)
+def using_shift(Array)
+  array.shift(array)
+end
+
+def shift_with_args(Array)
+  array.shift(2)
 end
